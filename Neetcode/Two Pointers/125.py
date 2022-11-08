bac1ad03c28bd6930e5bf860e0e6e837a38bfa55
ss <JonaSahota@hotmail.com>
2022-11-08 10:14:03 -0500

    complete solution

--- Neetcode/Two Pointers/125.py
+++ Neetcode/Two Pointers/125.py
@@ -1,9 +1,13 @@
 def isPalindrome(s: str):
     newstr = ""
+    oldstr = ""
+    for w in s:
+        if w.isalnum():
+            oldstr+=w
     for word in s[::-1]:
         if word.isalnum():
             newstr+=word
-    return newstr.lower() == s
+    return newstr.lower() == oldstr.lower()
 
 
 def main():
