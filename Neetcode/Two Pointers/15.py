fef7064dfaa5e69915543e31fe5d12f5455479ce
ss <JonaSahota@hotmail.com>
2022-11-10 21:57:44 -0500

    not complete yet

--- Neetcode/Two Pointers/15.py
+++ Neetcode/Two Pointers/15.py
@@ -1,14 +1,16 @@
 def threeSum(nums):
     # [-1,0,1,2,-1,-4]
     # [[-1,-1,2],[-1,0,1]]
+    new_arr = nums
     sums = []*3
     i = 0
     triplets = 0
     three_arrs = 0
     while(i!=0):
         if(checkZero(sum[three_arrs])!=0):
-            sums[three_arrs].append(nums[i])
+            sums[three_arrs].append(new_arr.remove(new_arr[i]))
             triplets +=1
+        # no progress yet, just need commit.
     
 
 
