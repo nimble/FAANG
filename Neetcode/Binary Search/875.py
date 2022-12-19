fca3378014b800fed434175f3ac6cc10b01c074a
ss <JonaSahota@hotmail.com>
2022-12-19 01:41:38 -0500

    lol

---
+++ Neetcode/Binary Search/875.py
@@ -0,0 +1,20 @@
+def minEatingSpeed(piles, h):
+    min = 0
+    hours = h
+    while( )
+
+
+    # [3,6,7,11]
+    # Pile ^
+    # bananas-per-hour eating speed = k
+    # no fucking clue what this question is asking LOL
+
+    return
+
+
+def main():
+    print(minEatingSpeed([3,6,7,11], 8))
+
+
+if __name__ == "__main__":
+    main()
