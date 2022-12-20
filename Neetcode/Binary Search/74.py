e9ae8383308aa31e9827ac2b4d1a29a909d43907
ss <JonaSahota@hotmail.com>
2022-12-20 00:35:23 -0500

    dummy commit

--- Neetcode/Binary Search/74.py
+++ Neetcode/Binary Search/74.py
@@ -5,8 +5,6 @@ def searchMatrix(matrix, target):
                 if(v == target):
                     print(f"Found at index {i}")
 
-
-
 def main():
     print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
 
