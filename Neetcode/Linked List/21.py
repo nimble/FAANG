8ac1d922c6fec049f8adaa1429e98e01215ab29b
J <21958992+ss@users.noreply.github.com>
2023-01-10 00:39:44 -0500

    merging linked lists completed

--- Neetcode/Linked List/21.py
+++ Neetcode/Linked List/21.py
@@ -6,7 +6,22 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         resultList = ListNode()
+        tail = resultList
           
         while(list1 and list2):
             if(list1.val < list2.val):
-                resultList.
+                tail.next = list1
+                l1 = l1.next
+            elif(list1.val > list2.val):
+                tail.next = list2
+                l2 = l2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        
+        if list2:
+            tail.next = list2
+        
+        
+        return resultList.next
+                
